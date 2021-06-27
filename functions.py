226e08c4167dd321945941411e4160be4e98a556
arshedgithub <arshedahmedmail@gmail.com>
2021-06-27 20:14:15 +0530

    first commit

---
+++ functions.py
@@ -0,0 +1,22 @@
+def addition():
+    a = float(input("Enter the First Operand : "))
+    b = float(input("Enter the Second Operand : "))
+    print(a+b)
+
+
+def substitution():
+    a = float(input("Enter the First Operand : "))
+    b = float(input("Enter the Second Operand : "))
+    print(a-b)
+
+
+def multiplication():
+    a = float(input("Enter the First Operand : "))
+    b = float(input("Enter the Second Operand : "))
+    print(a*b)
+
+
+def division():
+    a = float(input("Enter the First Operand : "))
+    b = float(input("Enter the Second Operand : "))
+    print(a*b)

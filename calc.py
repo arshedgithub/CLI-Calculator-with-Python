35308e2ded9dc5d86603d1c92a39dd9750cb0ab3
arshedgithub <arshedahmedmail@gmail.com>
2021-06-27 20:31:48 +0530

    initial commit

--- calc.py
+++ calc.py
@@ -5,20 +5,23 @@ print("Welcome to My Python Calculator")
 while True:
     print("\nEnter\t'a' for Addition\t's' for Substitution\t'm' for Multiplication\t'd' for Division")
     print("Enter q for Quit")
+
     c = input("Enter the Letter : ")
+
     if c == 'a':
-        print("Addition")
         functions.addition()
+
     elif c == 's':
-        print("substitution")
         functions.substitution()
+
     elif c == 'm':
-        print("Multiplication")
         functions.multiplication()
+
     elif c == 'd':
-        print("Division")
         functions.division()
+
     elif c == 'q':
         break
+
     else:
         print("Sorry, Invalid Input\n")
--- functions.py
+++ functions.py
@@ -1,22 +1,26 @@
 def addition():
+    print("Addition")
     a = float(input("Enter the First Operand : "))
     b = float(input("Enter the Second Operand : "))
     print(a+b)
 
 
 def substitution():
+    print("Substitution")
     a = float(input("Enter the First Operand : "))
     b = float(input("Enter the Second Operand : "))
     print(a-b)
 
 
 def multiplication():
+    print("Multiplication")
     a = float(input("Enter the First Operand : "))
     b = float(input("Enter the Second Operand : "))
     print(a*b)
 
 
 def division():
+    print("Division")
     a = float(input("Enter the First Operand : "))
     b = float(input("Enter the Second Operand : "))
     print(a*b)
